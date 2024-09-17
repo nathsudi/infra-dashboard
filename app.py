7ae35c3c61eeefda91c357803158c47c3cb1573a
Nath, Sudipta Kumar <sudipta.kumar.nath@intel.com>
2024-09-16 23:37:37 -0700

    integration of app.py to controller layer

--- app.py
+++ app.py
@@ -1,30 +1,8 @@
-from flask import Flask, render_template
-import json
+from flask import Flask
+from controllers.cluster_controller import cluster_blueprint
 
-#TODO: 1. move the routing to controller layer
-#TODO: 2. move the data processing to service like layer
-#TODO: 3. move route names to constant file
 app = Flask(__name__)
-
-# Load the data from JSON files
-def load_json_data(file_path):
-    with open(file_path, 'r') as f:
-        return json.load(f)
-
-@app.route('/')
-def clusters():
-    clusters_data = load_json_data('data/cluster-info.json')
-    return render_template('clusters.html', clusters=clusters_data['clusters'])
-
-@app.route('/hosts/<serial_number>')
-def hosts(serial_number):
-    hosts_data = load_json_data('data/hosts-info.json')
-    # Find the host with the matching 'serial_number' value
-    host_details = next((host for host in hosts_data['hosts'] if host['host-serial'] == serial_number), None)
-    if host_details:
-        return render_template('hosts.html', host=host_details)
-    else:
-        return "Host not found", 404
+app.register_blueprint(cluster_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
